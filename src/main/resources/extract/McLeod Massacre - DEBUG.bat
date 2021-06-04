6679553b4589d0fcdbadc3cfca2cb3d13f706820
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2021-06-04 21:22:51 +0200

    Fixed launcher issues

--- src/main/resources/extract/McLeod Massacre - DEBUG.bat
+++ src/main/resources/extract/McLeod Massacre - DEBUG.bat
@@ -1,3 +1,3 @@
 for %%a in (*.jar) do set "filename=%%a"
-java.exe -Djava.library.path=lib/ -jar "%filename%" --debug
+java.exe -Djava.library.path=lib/ -cp "%filename%" dartproductions.mcleodmassacre.Main --debug
 PAUSE
--- src/main/resources/extract/McLeod Massacre.bat
+++ src/main/resources/extract/McLeod Massacre.bat
@@ -1,2 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFC5HSRa+GGS5E7gZ5vzo08OJtkgRWOsVSJ3U0LGNNPMv+lfrf5Muwjd3u+YwGBpKeRe/Ug4kuXpHpXC5PsSTvUHoSUfp
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZks0
+::ZQ05rAF9IBncCkqN+0xwdVsEAlXMbAs=
+::ZQ05rAF9IAHYFVzEqQIdKzdRQwDCEWqpFbQf+u2b
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIDPBpaSAWQOCu2B6AS6+D+/aqBrV9dY+sxfIDMydQ=
+::dhA7uBVwLU+EWH2F5lZwGAlbSBGBKGK1CKZ8
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFC5HSRa+GGS5E7gZ5vzo08OJtkgRWOsVSJ3U0LGNNPMv+lfrf5Muwjd3u+YwHwlbQh+qZwEIu2tRuXGXNMKPjwr5XlqI5V8MNmZYgmzUzAY6c8BpmdEGnSWm+S0=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 for %%a in (*.jar) do set "filename=%%a"
-start javaw.exe -Djava.library.path=lib/ -jar "%filename%"
+start javaw.exe -Djava.library.path=lib/ -cp "%filename%" dartproductions.mcleodmassacre.Main
