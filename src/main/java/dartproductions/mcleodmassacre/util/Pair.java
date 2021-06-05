89a27073a23d5a9ef9c9fe816ecb5b0e7a8a95c9
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2021-06-05 21:35:24 +0200

    Added more Pair implementations

--- src/main/java/dartproductions/mcleodmassacre/util/Pair.java
+++ src/main/java/dartproductions/mcleodmassacre/util/Pair.java
@@ -1,79 +1,257 @@
 package dartproductions.mcleodmassacre.util;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 /**
- * Generic pair implementation
+ * Generic interface for storing pairs
  *
  * @param <First>  The type of the first value
  * @param <Second> The type of the second value
+ * @since 0.1.0
  */
-public final class Pair<First, Second> {
+public interface Pair<First, Second> {
 	/**
-	 * The first value
-	 */
-	public @Nullable First first;
-	/**
-	 * The second value
-	 */
-	public @Nullable Second second;
-	
-	/**
-	 * Creates a new pair.
+	 * Gets the first value of the pair
 	 *
-	 * @param first  The first value
-	 * @param second The second value
+	 * @return The first value
+	 * @since 0.1.0
 	 */
-	public Pair(@Nullable First first, @Nullable Second second) {
-		this.first = first;
-		this.second = second;
+	public default @Nullable First getFirst() {
+		return first();
 	}
 	
 	/**
-	 * Creates a new pair with null values.
+	 * Gets the second value of the pair
+	 *
+	 * @return The first value
+	 * @since 0.1.0
 	 */
-	public Pair() {
+	public default @Nullable Second getSecond() {
+		return second();
 	}
 	
 	/**
 	 * Gets the first value of the pair
 	 *
 	 * @return The first value
+	 * @since 0.1.0
 	 */
-	public @Nullable First first() {
-		return first;
-	}
+	public @Nullable First first();
 	
 	/**
 	 * Gets the second value of the pair
 	 *
-	 * @return The second value
+	 * @return The first value
+	 * @since 0.1.0
 	 */
-	public @Nullable Second second() {
-		return second;
+	public @Nullable Second second();
+	
+	/**
+	 * Mutable pair implementation
+	 *
+	 * @since 0.1.0
+	 */
+	public static class MutablePair<First, Second> implements Pair<First, Second> {
+		/**
+		 * The first value
+		 *
+		 * @since 0.1.0
+		 */
+		protected @Nullable First first;
+		/**
+		 * The second value
+		 *
+		 * @since 0.1.0
+		 */
+		protected @Nullable Second second;
+		
+		/**
+		 * Creates a new pair.
+		 *
+		 * @param first  The first value
+		 * @param second The second value
+		 * @since 0.1.0
+		 */
+		public MutablePair(@Nullable First first, @Nullable Second second) {
+			this.first = first;
+			this.second = second;
+		}
+		
+		/**
+		 * Creates a new pair with null values.
+		 *
+		 * @since 0.1.0
+		 */
+		public MutablePair() {
+		}
+		
+		@Override
+		public @Nullable First first() {
+			return first;
+		}
+		
+		@Override
+		public @Nullable Second second() {
+			return second;
+		}
+		
+		/**
+		 * Sets the first value of the pair
+		 *
+		 * @param first The new first value
+		 * @return The previous first value
+		 * @since 0.1.0
+		 */
+		public @Nullable First setFirst(@Nullable First first) {
+			First prev = this.first;
+			this.first = first;
+			return prev;
+		}
+		
+		/**
+		 * Sets the second value of the pair
+		 *
+		 * @param second The new second value
+		 * @return The previous second value
+		 * @since 0.1.0
+		 */
+		public @Nullable Second setSecond(@Nullable Second second) {
+			Second prev = this.second;
+			this.second = second;
+			return prev;
+		}
+		
+		/**
+		 * Null-safe immutable pair implementation
+		 *
+		 * @since 0.1.0
+		 */
+		public static class MutableNullsafePair<First, Second> extends MutablePair<First, Second> {
+			/**
+			 * Creates a new pair.
+			 *
+			 * @param first  The first value
+			 * @param second The second value
+			 * @since 0.1.0
+			 */
+			public MutableNullsafePair(@NotNull First first, @NotNull Second second) {
+				super(Objects.requireNonNull(first), Objects.requireNonNull(second));
+			}
+			
+			@Override
+			public @NotNull First first() {
+				return Objects.requireNonNull(super.first());
+			}
+			
+			@Override
+			public @NotNull Second second() {
+				return Objects.requireNonNull(super.second());
+			}
+			
+			/**
+			 * Sets the first value of the pair
+			 *
+			 * @param first The new first value
+			 * @return The previous first value
+			 * @since 0.1.0
+			 */
+			public @NotNull First setFirst(@Nullable First first) {
+				Objects.requireNonNull(first);
+				return Objects.requireNonNull(super.setFirst(first));
+			}
+			
+			/**
+			 * Sets the second value of the pair
+			 *
+			 * @param second The new second value
+			 * @return The previous second value
+			 * @since 0.1.0
+			 */
+			public @NotNull Second setSecond(@Nullable Second second) {
+				Objects.requireNonNull(second);
+				return Objects.requireNonNull(super.setSecond(second));
+			}
+		}
 	}
 	
 	/**
-	 * Sets the first value of the pair
+	 * Immutable pair implementation
 	 *
-	 * @param first The new first value
-	 * @return The previous first value
+	 * @since 0.1.0
 	 */
-	public @Nullable First setFirst(@Nullable First first) {
-		First prev = this.first;
-		this.first = first;
-		return prev;
-	}
-	
-	/**
-	 * Sets the second value of the pair
-	 *
-	 * @param second The new second value
-	 * @return The previous second value
-	 */
-	public @Nullable Second setSecond(@Nullable Second second) {
-		Second prev = this.second;
-		this.second = second;
-		return prev;
+	public static class ImmutablePair<First, Second> implements Pair<First, Second> {
+		/**
+		 * The first value
+		 *
+		 * @since 0.1.0
+		 */
+		protected @Nullable First first;
+		/**
+		 * The second value
+		 *
+		 * @since 0.1.0
+		 */
+		protected @Nullable Second second;
+		
+		/**
+		 * Creates a new pair.
+		 *
+		 * @param first  The first value
+		 * @param second The second value
+		 * @since 0.1.0
+		 */
+		public ImmutablePair(@Nullable First first, @Nullable Second second) {
+			this.first = first;
+			this.second = second;
+		}
+		
+		/**
+		 * Creates a new pair with null values.
+		 *
+		 * @since 0.1.0
+		 */
+		public ImmutablePair() {
+		}
+		
+		@Override
+		public @Nullable First first() {
+			return first;
+		}
+		
+		@Override
+		public @Nullable Second second() {
+			return second;
+		}
+		
+		/**
+		 * Null-safe immutable pair implementation
+		 *
+		 * @since 0.1.0
+		 */
+		public static class ImmutableNullsafePair<First, Second> extends ImmutablePair<First, Second> {
+			/**
+			 * Creates a new pair.
+			 *
+			 * @param first  The first value
+			 * @param second The second value
+			 * @since 0.1.0
+			 */
+			public ImmutableNullsafePair(@NotNull First first, @NotNull Second second) {
+				super(Objects.requireNonNull(first), Objects.requireNonNull(second));
+			}
+			
+			@Override
+			public @NotNull First first() {
+				return Objects.requireNonNull(first);
+			}
+			
+			@Override
+			public @NotNull Second second() {
+				return Objects.requireNonNull(second);
+			}
+		}
 	}
 }
