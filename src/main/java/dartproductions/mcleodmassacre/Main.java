1e4e872212eef5335c5f80db3b28e4109a8e1b9a
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2021-06-07 21:57:23 +0200

    Added debugging utilities

--- src/main/java/dartproductions/mcleodmassacre/Main.java
+++ src/main/java/dartproductions/mcleodmassacre/Main.java
@@ -12,9 +12,13 @@ import org.apache.logging.log4j.core.config.Configurator;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 /**
  * Main application class, launches the game, handles global game states
@@ -33,7 +37,19 @@ public class Main {
 	 *
 	 * @since 0.1.0
 	 */
-	private static final @NotNull ExecutorService EXECUTORS = Executors.newFixedThreadPool(4, Executors.privilegedThreadFactory());
+	private static final @NotNull ExecutorService EXECUTORS = Executors.newFixedThreadPool(4, new ThreadFactory() {
+		protected int count = 0;
+		
+		@Override
+		public Thread newThread(@NotNull Runnable r) {
+			Thread t = new Thread(r, "Executor " + (count++));
+			t.setUncaughtExceptionHandler((t1, e) -> {
+				LOGGER.error("Uncaught exception in async executor thread (" + t1.getName() + ")", e);
+				Main.panic();
+			});
+			return t;
+		}
+	});
 	/**
 	 * True if additional debug information should be logged. Defaults to false.
 	 *
@@ -100,6 +116,27 @@ public class Main {
 	private static void configureLogger() {
 		System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
 		Configurator.setAllLevels("", isDebug() ? Level.DEBUG : Level.INFO);
+		System.setOut(createLoggingProxy(System.out, Level.DEBUG));
+		System.setErr(createLoggingProxy(System.err, Level.WARN));
+		if(isDebug()) {
+			printSystemDebugInfo();
+		}
+	}
+	
+	/**
+	 * Creates a new {@link PrintStream} from the specified stream that writes to the log output.
+	 *
+	 * @param stream The original stream
+	 * @param level  The level of the logged messages
+	 * @return The new logging stream
+	 * @since 0.1.0
+	 */
+	private static PrintStream createLoggingProxy(final PrintStream stream, final Level level) {
+		return new PrintStream(stream) {
+			public void print(final String string) {
+				LOGGER.log(level, string.strip());
+			}
+		};
 	}
 	
 	/**
@@ -204,10 +241,88 @@ public class Main {
 	public static synchronized void setGameState(@NotNull GameState newGameState, @Nullable GameState newNextState) {
 		GAME_STATE = newGameState;
 		NEXT_STATE = newNextState;
-		LOGGER.info("Changed state to " + GAME_STATE + (newNextState == null ? "" : " (with next state " + newNextState + ")"));
+		LOGGER.debug("Changed state to " + GAME_STATE + (newNextState == null ? "" : " (with next state " + newNextState + ")"));
 		GameEngine.onStateChange(newGameState, newNextState);
 		GraphicsManager.onStateChange(newGameState, newNextState);
 		SoundManager.onStateChange(newGameState, newNextState);
+	}
+	
+	/**
+	 * Prints debug information about the underlying OS, the java instance, and the runtime in general.
+	 *
+	 * @since 0.1.0
+	 */
+	public static void printSystemDebugInfo() {
+		LOGGER.info("\n=======DEBUG INFO=======\n");
+		LOGGER.info("This data is only queried in debug mode or after a crash, and doesn't contain any personal information. It is used for debugging purposes only.");
+		LOGGER.info("\n---ENVIRONMENT INFO---");
+		String[] properties = new String[]{"file.encoding", "java.class.path", "java.class.version", "java.library.path", "java.runtime.name", "java.runtime.version", "java.specification.name", "java.specification.vendor", "java.specification.version", "java.vendor", "java.vendor.url", "java.version", "java.version.date", "java.vm.compressedOopsMode", "java.vm.info", "java.vm.name", "java.vm.specification.name", "java.vm.specification.vendor", "java.vm.specification.version", "java.vm.vendor", "java.vm.version", "jdk.debug", "os.arch", "os.name", "os.version", "sun.arch.data.model", "sun.cpu.endian", "sun.cpu.isalist", "sun.io.unicode.encoding", "sun.java.command", "sun.java.launcher", "sun.jnu.encoding", "sun.management.compiler", "sun.stderr.encoding", "sun.stdout.encoding"};
+		for(String property : properties) {
+			LOGGER.info(property + ": " + System.getProperty(property));
+		}
+		LOGGER.info("\n---RUNTIME INFO---");
+		LOGGER.info("Available processors: " + Runtime.getRuntime().availableProcessors());
+		LOGGER.info("Free memory: " + Runtime.getRuntime().freeMemory());
+		LOGGER.info("Total memory: " + Runtime.getRuntime().totalMemory());
+		LOGGER.info("Maximum memory: " + Runtime.getRuntime().maxMemory());
+		try {
+			com.sun.management.OperatingSystemMXBean os = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+			LOGGER.info("\n---OS INFO---");
+			LOGGER.info("Committed virtual memory size: " + os.getCommittedVirtualMemorySize());
+			LOGGER.info("Free memory: " + os.getFreeMemorySize());
+			LOGGER.info("Free swap space: " + os.getFreeSwapSpaceSize());
+			LOGGER.info("Total memory size: " + os.getTotalMemorySize());
+			LOGGER.info("Total swap space: " + os.getTotalSwapSpaceSize());
+		} catch(Exception e) {
+			LOGGER.info("Could not query further OS information (build version, available memory etc). Please make sure to include the exact version of your OS (with the specific build id) in the description of any bug report.");
+		}
+		LOGGER.info("\n=======END OF DEBUG INFO=======\n");
+	}
+	
+	/**
+	 * Prints debug information about the cpu's and the system's utilization in general.
+	 *
+	 * @since 0.1.0
+	 */
+	public static void printSystemUsageDebugInfo() {
+		try {
+			OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
+			LOGGER.info("\n---SYSTEM USAGE INFO---");
+			if(os instanceof com.sun.management.OperatingSystemMXBean osBean) {
+				LOGGER.info("CPU load: " + osBean.getCpuLoad());
+				LOGGER.info("Process CPU load: " + osBean.getProcessCpuLoad());
+				LOGGER.info("Process CPU time: " + osBean.getProcessCpuTime());
+			}
+			LOGGER.info("Average system load: " + os.getSystemLoadAverage());
+			LOGGER.info("\n---END OF SYSTEM USAGE INFO---");
+		} catch(Exception e) {
+			LOGGER.info("Could not query exact system usage information", e);
+		}
+	}
+	
+	/**
+	 * Dumps the thread stacks to the output and the logger.
+	 *
+	 * @since 0.1.0
+	 */
+	public static void printThreadDump() {
+		LOGGER.info("\n---THREAD DUMP---");
+		Thread.getAllStackTraces().forEach((thread, stackTrace) -> {
+			if(thread != null && stackTrace.length > 0) {
+				LOGGER.info("Dumping thread " + thread.getName());
+				LOGGER.info(stackTrace[0]);
+				for(int i = 1; i < stackTrace.length; i++) {
+					LOGGER.info("\tat " + stackTrace[i]);
+				}
+			}
+		});
+		LOGGER.info("\n---END OF THREAD DUMP---");
+	}
+	
+	public static void panic() {
+		printSystemUsageDebugInfo();
+		printSystemDebugInfo();
+		printThreadDump();
 	}
 	
 	/**
@@ -287,6 +402,12 @@ public class Main {
 		 *
 		 * @since 0.1.0
 		 */
-		VERSUS_MENU
+		VERSUS_MENU,
+		/**
+		 * The credits screen
+		 *
+		 * @since 0.1.0
+		 */
+		CREDITS
 	}
 }
