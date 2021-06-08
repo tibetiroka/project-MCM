f4b1b32625511ebcc360ded3efd6a59dca09450c
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2021-06-08 21:18:01 +0200

    Updated docs

---
+++ src/main/java/dartproductions/mcleodmassacre/options/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains classes for managing the game's settings.
+ *
+ * @author tibetiroka
+ * @since 0.1.0
+ */
+package dartproductions.mcleodmassacre.options;
---
+++ src/main/java/dartproductions/mcleodmassacre/sound/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the sound engine. The game currently uses the standard Java Sound API.
+ * <p>
+ * The sound engine is based on the class {@link dartproductions.mcleodmassacre.sound.SoundManager}
+ *
+ * @author tibetiroka
+ * @since 0.1.0
+ */
+package dartproductions.mcleodmassacre.sound;
