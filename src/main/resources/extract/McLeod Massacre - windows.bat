b96b6b2ec94c425ab79ec7e79555627e78955b45
tibetiroka <68112292+tibetiroka@users.noreply.github.com>
2021-06-13 19:29:44 +0200

    Fixed license header in windows launcher

--- src/main/resources/extract/McLeod Massacre - windows.bat
+++ src/main/resources/extract/McLeod Massacre - windows.bat
@@ -1,9 +1,9 @@
-Rem +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Rem Copyright (c) 2021 Dart Productions
-Rem Released under the GNU General Public License version 3
-Rem
-Rem This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
-Rem +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+:: +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+:: Copyright (c) 2021 Dart Productions
+:: Released under the GNU General Public License version 3
+::
+:: This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation.
+:: +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 for %%a in (*.jar) do set "filename=%%a"
 start javaw.exe -Djava.library.path=lib/ -cp "%filename%" dartproductions.mcleodmassacre.Main
